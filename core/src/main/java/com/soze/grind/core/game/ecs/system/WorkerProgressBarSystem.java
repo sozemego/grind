ccf1e18be7abe424e46bedd770bd995f27daa06d
sozemego <piknypiec@gmail.com>
2021-01-04 21:34:30 +0100

    ProgressBar update for Workers is now moved to its own WorkerProgressBarSystem system

---
+++ core/src/main/java/com/soze/grind/core/game/ecs/system/WorkerProgressBarSystem.java
@@ -0,0 +1,70 @@
+package com.soze.grind.core.game.ecs.system;
+
+import com.artemis.BaseEntitySystem;
+import com.artemis.ComponentMapper;
+import com.artemis.annotations.All;
+import com.artemis.utils.IntBag;
+import com.soze.grind.core.game.GameStage;
+import com.soze.grind.core.game.ecs.component.PositionComponent;
+import com.soze.grind.core.game.ecs.component.WorkerAiComponent;
+import com.soze.grind.core.game.ecs.component.WorkerAiComponent.WorkerState;
+import com.soze.grind.core.game.ecs.component.WorkerProgressBarComponent;
+import com.soze.grind.core.game.ui.ProgressBar;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@All({WorkerAiComponent.class, WorkerProgressBarComponent.class, PositionComponent.class})
+public class WorkerProgressBarSystem extends BaseEntitySystem {
+
+	private ComponentMapper<WorkerAiComponent> workerAiMapper;
+	private ComponentMapper<WorkerProgressBarComponent> workerProgressBarMapper;
+	private ComponentMapper<PositionComponent> positionMapper;
+
+	private final GameStage gameStage;
+
+	@Autowired
+	public WorkerProgressBarSystem(GameStage gameStage) {
+		this.gameStage = gameStage;
+	}
+
+	@Override
+	protected void inserted(int entityId) {
+
+		WorkerProgressBarComponent workerProgressBarComponent = workerProgressBarMapper.get(entityId);
+
+		gameStage.addActor(workerProgressBarComponent.getProgressBar());
+
+	}
+
+	@Override
+	protected void removed(int entityId) {
+		WorkerProgressBarComponent workerProgressBarComponent = workerProgressBarMapper.get(entityId);
+
+		workerProgressBarComponent.getProgressBar().remove();
+	}
+
+	@Override
+	protected void processSystem() {
+
+		IntBag activeEntities = getSubscription().getEntities();
+		int[] ids = activeEntities.getData();
+
+		for (int i = 0; i < activeEntities.size(); i++) {
+
+			int id = ids[i];
+
+			WorkerAiComponent workerAiComponent = workerAiMapper.get(id);
+			PositionComponent positionComponent = positionMapper.get(id);
+			WorkerProgressBarComponent workerProgressBarComponent = workerProgressBarMapper.get(id);
+
+			ProgressBar progressBar = workerProgressBarComponent.getProgressBar();
+
+			progressBar.setVisible(workerAiComponent.getState() == WorkerState.WORKING);
+			progressBar.setProgress(workerAiComponent.getWorkingProgress());
+			progressBar.setPosition(positionComponent.getX(), positionComponent.getY());
+
+		}
+	}
+
+}
--- core/src/main/java/com/soze/grind/core/game/ecs/world/WorldConfig.java
+++ core/src/main/java/com/soze/grind/core/game/ecs/world/WorldConfig.java
@@ -5,6 +5,7 @@ import com.artemis.WorldConfiguration;
 import com.artemis.WorldConfigurationBuilder;
 import com.soze.grind.core.game.ecs.system.ActorPositionSystem;
 import com.soze.grind.core.game.ecs.system.WorkerAiSystem;
+import com.soze.grind.core.game.ecs.system.WorkerProgressBarSystem;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +13,18 @@ import org.springframework.context.annotation.Configuration;
 public class WorldConfig {
 
   @Bean
-  public World world(ActorPositionSystem actorPositionSystem, WorkerAiSystem workerAiSystem) {
+  public World world(
+      ActorPositionSystem actorPositionSystem,
+      WorkerAiSystem workerAiSystem,
+      WorkerProgressBarSystem workerProgressBarSystem
+  ) {
 
     WorldConfiguration worldConfiguration =
-        new WorldConfigurationBuilder().with(actorPositionSystem).with(workerAiSystem).build();
+        new WorldConfigurationBuilder()
+            .with(actorPositionSystem)
+            .with(workerAiSystem)
+            .with(workerProgressBarSystem)
+            .build();
 
     World world = new World(worldConfiguration);
 
